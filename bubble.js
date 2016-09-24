1517c0bea6b1a71098979c0b91ca65f7b8db5d6e
zongying <1985.zong@gmail.com>
2016-09-24 21:39:06 +0800

    冒泡排序

--- bubble.js
+++ bubble.js
@@ -1,24 +1,22 @@
-function bubbleSort3(arr3) {
-
+function bubbleSort(arr) {
     var low = 0;
-    var high = arr.length - 1; //设置变量的初始值
+    var high= arr.length-1; //设置变量的初始值
     var tmp,j;
-    console.time('改进后冒泡排序耗时');
+    console.time('2.改进后冒泡排序耗时');
     while (low < high) {
         for (j= low; j< high; ++j) //正向冒泡,找到最大者
             if (arr[j]> arr[j+1]) {
                 tmp = arr[j]; arr[j]=arr[j+1];arr[j+1]=tmp;
             }
-        --high; //修改high值, 前移一位
-        for(j=high; j>low; --j) //反向冒泡,找到最小者
+        --high;                 //修改high值, 前移一位
+        for (j=high; j>low; --j) //反向冒泡,找到最小者
             if (arr[j]<arr[j-1]) {
                 tmp = arr[j]; arr[j]=arr[j-1];arr[j-1]=tmp;
             }
-        ++low;  //修改low值,后移一位
+        ++low;                  //修改low值,后移一位
     }
-    console.timeEnd('改进后冒泡排序耗时');
-    return arr3;
+    console.timeEnd('2.改进后冒泡排序耗时');
+    return arr;
 }
-
 var arr=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48];
-console.log(bubbleSort3(arr)); //[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
+console.log(bubbleSort(arr));//[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
